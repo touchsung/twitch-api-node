4eec44cd9d57e54f872defee64cf2a5189fb149f
Jettapat <jettapat@techupth.com>
2023-05-18 20:17:41 +0700

    feat: change feed by twitch chat

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const client = require("./utils/streamlabsOBSClient");
 const app = express();
+const tmi = require("tmi.js");
+
 const port = process.env.PORT || 3000;
 
-app.get("/feed-pet", async (req, res) => {
-  await client.connect();
-  client.increaseSourceScale("Pc", "Pet");
-  res.send("Success");
+const twitchConfig = {
+  channels: [process.env.TWITCH_CHANNEL],
+};
+
+const twitchClient = new tmi.client(twitchConfig);
+twitchClient.on("connected", () => {
+  console.log("Connected to Twitch chat");
 });
+
+twitchClient.connect();
+
+twitchClient.on("message", async (channel, tags, message, self) => {
+  if (message.toLocaleLowerCase() === "!feed") {
+    await client.connect();
+    client.increaseSourceScale("Pc", "Pet");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
